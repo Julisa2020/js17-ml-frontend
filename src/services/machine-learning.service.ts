efa29667beefacdedbba8c7277c9fc100fe6cabf
doha9900 <dhuarez.arevalo@gmail.com>
2021-07-31 04:47:19 -0500

    kmeans works

---
+++ src/services/machine-learning.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MachineLearningService {
+  // readonly URL = 'https://js17-ml-backend.herokuapp.com';
+  readonly URL = 'http://127.0.0.1:5000';
+
+  constructor(private http: HttpClient) { }
+  getData(){ return this.http.get(this.URL + '/kmeans'); }
+  runKmeans(form: any) {
+    return this.http.post(this.URL+ '/kmeans', form);
+  }
+
+}
