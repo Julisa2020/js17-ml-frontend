0cc5a0fa698b021930079c41109818426e16795e
julisa <milagrosjulisa.mm@gmail.com>
2021-07-31 18:54:58 -0500

    Prueba paginacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination'; 
 
 /* import { AppRoutingModule } from './app-routing.module'; */
 import { AppComponent } from './app.component';
@@ -18,7 +19,11 @@ import { KmeansComponent } from './kmeans/kmeans.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule
+  ],
+  exports: [
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
