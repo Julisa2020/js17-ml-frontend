e69fd9a38e72bda79a9d1d2b98cc0470f878f648
doha9900 <dhuarez.arevalo@gmail.com>
2021-08-14 10:29:17 -0500

    kmeans works in X columns

--- src/app/components/kmeans/kmeans.component.ts
+++ src/app/components/kmeans/kmeans.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { MachineLearningService } from '../../../services/machine-learning.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NgxPaginationModule } from 'ngx-pagination';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-kmeans',
@@ -12,7 +13,9 @@ import { NgxPaginationModule } from 'ngx-pagination';
 export class KmeansComponent implements OnInit {
   page: number;
   form: FormGroup;
+  response: any;
   data: any;
+  columns: any;
   img: any;
   showForm: boolean;
   showResults: boolean;
@@ -35,30 +38,48 @@ export class KmeansComponent implements OnInit {
 
   sendData() {
     this.form = this.formbuilder.group({
-      query: ['', [Validators.required, Validators.minLength(0)]],
-      column_1: ['', [Validators.required, Validators.minLength(0)]],
-      column_2: ['', [Validators.required, Validators.minLength(0)]],
-      columns: ['', [Validators.required, Validators.minLength(0)]],
-      n_clusters: [1, [Validators.required, Validators.min(1)]],
+      query: ["select o.htitulo_cat as categoria,o.htitulo as perfil, w.pagina_web,o.empresa,o.lugar,o.salario,date_part('year',o.fecha_publicacion) as periodo, f_dimPerfilOferta(o.id_oferta,7) as funciones, f_dimPerfilOferta(o.id_oferta,1) as conocimiento, f_dimPerfilOferta(o.id_oferta,3) as habilidades, f_dimPerfilOferta(o.id_oferta,2) as competencias, f_dimPerfilOferta(o.id_oferta,17) as certificaciones, f_dimPuestoEmpleo(o.id_oferta,5) as beneficio, f_dimPerfilOferta(o.id_oferta,11) as formacion from webscraping w inner join oferta o on (w.id_webscraping=o.id_webscraping) where o.id_estado is null limit 500;", [Validators.required, Validators.minLength(0)]],
+      // columns: ['', [Validators.required, Validators.minLength(0)]],
+      n_clusters: [5, [Validators.required, Validators.min(1)]],
       init: ['', [Validators.required, Validators.minLength(1)]],
-      max_iter: [0, [Validators.required, Validators.min(1)]],
+      max_iter: [500, [Validators.required, Validators.min(1)]],
+      // query: ['', [Validators.required, Validators.minLength(0)]],
+      // column_1: ['', [Validators.required, Validators.minLength(0)]],
+      // column_2: ['', [Validators.required, Validators.minLength(0)]],
+      // columns: ['', [Validators.required, Validators.minLength(0)]],
+      // n_clusters: [1, [Validators.required, Validators.min(1)]],
+      // init: ['', [Validators.required, Validators.minLength(1)]],
+      // max_iter: [0, [Validators.required, Validators.min(1)]],
     });
   }
 
   runKmeans(){
-    let column_1 = this.form.get('column_1')?.value;
-    let column_2 = this.form.get('column_2')?.value;
-    let columns= [];
-    columns.push(column_1, column_2);
-    this.form.controls["columns"].setValue(columns);
+    Swal.fire({
+          title: 'Cargando ...',
+          // allowOutsideClick: false
+    });
+    Swal.showLoading();
+    // let column_1 = this.form.get('column_1')?.value;
+    // let column_2 = this.form.get('column_2')?.value;
+    // let columns= [];
+    // columns.push(column_1, column_2);
+    // this.form.controls["columns"].setValue(columns);
     this.machineLearningService.runKmeans(this.form.value).subscribe((result: any)=>{
-      // console.log(result)
+      Swal.close();
       this.showResults = true;
-      this.data = result;
+      this.response = result;
+      console.log(this.response);
+      this.data = result?.data.data;
+      this.columns = result?.columns.filter((item:any) => item !== "cluster");
       this.img = 'data:image/jpg;base64,'
-                 + this.data?.graphic;
+                 + this.response?.graphic;
     }, (err:any)=>{
-      alert("Ocurrió un error :(");
+      Swal.close();
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '!Ocurrió un error!',
+      })
     });
   }
 }
